1c0769e0280165f7e7fa19e45a9d6a4afc6f1dc7
Barry G. Becker <barrybecker4@yahoo.com>
2015-12-16 10:36:14 -0800

    Move more java classes to scala.

---
+++ src/main/scala/com/barrybecker4/simulation/fractalexplorer/algorithm/History.scala
@@ -0,0 +1,23 @@
+/** Copyright by Barry G. Becker, 2015. Licensed under MIT License: http://www.opensource.org/licenses/MIT  */
+package com.barrybecker4.simulation.fractalexplorer.algorithm
+
+import com.barrybecker4.common.math.ComplexNumberRange
+
+/**
+  * Maintains regions (ranges) in the complex space that were visited, so we can back up.
+  * @author Barry Becker
+  */
+class History {
+  /** range of bounding box in complex plane. */
+  private var stack: List[ComplexNumberRange] = List()
+
+  def addRangeToHistory(range: ComplexNumberRange) = stack = range :: stack
+  def hasHistory: Boolean = stack.nonEmpty
+
+  /** @return the last range viewed in the history */
+  def popLastRange: ComplexNumberRange = {
+    val head = stack.head
+    stack = stack.tail
+    head
+  }
+}
