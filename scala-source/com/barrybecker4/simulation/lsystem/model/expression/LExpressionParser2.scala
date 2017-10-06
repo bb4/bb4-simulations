c3a93a34318721ff2fc80f3050edcb3586a30f26
barrybecker4 <barrybecker4@yahoo.com>
2017-10-06 02:46:09 -0700

    Experiment with scala combinator parsing. Got replacement working. Not yet integrated. WIP

--- scala-source/com/barrybecker4/simulation/lsystem/model/expression/LExpressionParser2.scala
+++ scala-source/com/barrybecker4/simulation/lsystem/model/expression/LExpressionParser2.scala
@@ -2,32 +2,31 @@
 package com.barrybecker4.simulation.lsystem.model.expression
 
 import com.barrybecker4.common.expression.TreeNode
-
 import scala.util.parsing.combinator._
-//import java.util.List
 import scala.collection.JavaConverters._
 
-
+/**
+  * Parses the text form of an L-system expression into a tree representation.
+  * See https://en.wikipedia.org/wiki/L-system
+  * I originally implemented a custom parser, but this version leverages scala's parser combinator library.
+  * @author Barry Becker
+  */
 class LExpressionParser2 extends RegexParsers {
 
   val ops = new LOperatorsDefinition
 
-
   def plusFactor: Parser[Seq[TreeNode]] = "+" ~ factor ^^ { case "+" ~ f => Seq(new TreeNode("+", ops)) ++ f}
   def minusFactor: Parser[Seq[TreeNode]] = "-" ~ factor ^^ { case "-" ~ f => Seq(new TreeNode("-", ops)) ++ f}
   def theExp: Parser[Seq[TreeNode]] = exp ^^ { e => Seq(e) }
-
   def factor: Parser[Seq[TreeNode]] = rep(theExp | plusFactor | minusFactor) ^^ { _.flatten.toSeq }
-
 
   def exp: Parser[TreeNode] = ("F".r | "("~>factor<~")") ^^ {
     case f: String => new TreeNode("F", ops)
-    case fact: Seq[TreeNode] => {
+    case fact: Seq[TreeNode] =>
       val tn = new TreeNode("", ops)
       tn.hasParens = true
       tn.children = fact.asJava
       tn
-    }
     case x => throw new UnsupportedOperationException("Unexpected: " + x.getClass.getName )
   }
 
@@ -35,14 +34,12 @@ class LExpressionParser2 extends RegexParsers {
     val parsed: ParseResult[Seq[TreeNode]] = parseAll(factor, expression)
     println("parsed = " + parsed)
 
-    if (parsed.isEmpty) return None
-    if (parsed.get.length > 1) {
+    if (parsed.isEmpty) None
+    else if (parsed.get.length > 1) {
       val root = new TreeNode("", ops)
       //root.hasParens = true
       root.children = parsed.get.asJava
       Some(root)
-    } else {
-      Some(parsed.get.head)
-    }
+    } else Some(parsed.get.head)
   }
 }
