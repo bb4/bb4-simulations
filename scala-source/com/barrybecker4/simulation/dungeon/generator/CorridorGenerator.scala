b9c4e70d30c6b9ec7daa0abcedff5a4d44bd23ef
barrybecker4 <barrybecker4@gmail.com>
2021-12-22 08:56:14 -0800

    continue implementing corridorGen

--- scala-source/com/barrybecker4/simulation/dungeon/generator/CorridorGenerator.scala
+++ scala-source/com/barrybecker4/simulation/dungeon/generator/CorridorGenerator.scala
@@ -41,14 +41,19 @@ case class CorridorGenerator(options: DungeonOptions) {
     // find all rightmost nodes in leftNode and leftMost in rightNode
     // add at let one connection based on value of options.connectivity
     // might need to broaden the range searched for until wer find at least one room
-    val rightEdgeBox = Box(1, 2, 3, 4)
+    val rightEdgeBox = Box(0, splitPos - options.getMinPaddedDim, options.dimension.height, splitPos)
     val leftRooms = findRoomsGivenBoxFilter(rightEdgeBox, leftNode)
-    val leftEdgeBox = Box(1, 2, 3, 4)
+    val leftEdgeBox = Box(0, splitPos, options.dimension.height, splitPos + - options.getMinPaddedDim)
     val rightRooms = findRoomsGivenBoxFilter( leftEdgeBox, rightNode)
+    // add the connections between left and right rooms
   }
 
-  private def addVerticalConnections(splitPos: Int, leftNode: BspNode[Room], rightNode: BspNode[Room]): Set[Room] = {
-    Set()
+  private def addVerticalConnections(splitPos: Int, topNode: BspNode[Room], bottomNode: BspNode[Room]): Unit = {
+    val bottomEdgeBox = Box(splitPos - options.getMinPaddedDim, 0, splitPos, options.dimension.width)
+    val topRooms = findRoomsGivenBoxFilter(bottomEdgeBox, topNode)
+    val topEdgeBox = Box(splitPos, 0, splitPos + options.getMinPaddedDim, options.dimension.height)
+    val bottomRooms = findRoomsGivenBoxFilter( topEdgeBox, bottomNode)
+    // add the connections between top and bottom rooms
   }
 
   private def findRoomsGivenBoxFilter(boxFilter: Box, bottomNode: BspNode[Room]): Set[Room] = {
