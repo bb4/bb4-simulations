d671776b1fcb1e617cfce903308049524145c261
barrybecker4 <barrybecker4@gmail.com>
2021-12-23 07:56:11 -0800

    move man generatrion helper classes to subpackage

--- scala-test/com/barrybecker4/simulation/dungeon/generator/processors/BoxSplitterSuite.scala
+++ scala-test/com/barrybecker4/simulation/dungeon/generator/processors/BoxSplitterSuite.scala
@@ -1,8 +1,8 @@
 // Copyright by Barry G. Becker, 2021. Licensed under MIT License: http://www.opensource.org/licenses/MIT
-package com.barrybecker4.simulation.dungeon.generator
+package com.barrybecker4.simulation.dungeon.generator.processors
 
 import com.barrybecker4.common.geometry.Box
-import com.barrybecker4.simulation.dungeon.generator.bsp.BoxSplitter
+import com.barrybecker4.simulation.dungeon.generator.processors.BoxSplitter
 import org.scalatest.funsuite.AnyFunSuite
 
 import scala.util.Random
