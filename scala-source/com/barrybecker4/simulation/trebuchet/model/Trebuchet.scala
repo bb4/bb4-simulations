ebda02278be5d396bd8a6e36a604bcd4c29d8b0f
Barry Becker <barrybecker4@gmail.com>
2022-05-01 09:05:46 -0700

    split out TrebuchetProcessor

--- scala-source/com/barrybecker4/simulation/trebuchet/model/Trebuchet.scala
+++ scala-source/com/barrybecker4/simulation/trebuchet/model/Trebuchet.scala
@@ -23,33 +23,19 @@ import java.lang.Math.sin
 
 /**
   * Data structure and methods for representing a dynamic trebuchet
-  * The geometry of the trebuchet is defined by constants in TebuchetConstants.
-  *
-  * Performance Improvements:
-  *    - profile (where is the time spent? rendering or computation)
-  *
-  * @author Barry Becker
-  */
-object Trebuchet {
-  private val GRAVITY_VEC = new Vector2d(0, GRAVITY)
-  private val MAX_LEVER_ANGLE = PI - 0.1
-}
-
-/**
-  * Use a hard-coded static data interface to initialize
-  * so it can be easily run in an applet without using resources.
+  * The geometry of the trebuchet is defined by constants in TebuchetConstants
   */
 class Trebuchet(var showVelocityVectors: Boolean = false, var showForceVectors: Boolean = false) {
 
+  // scales the geometry of the trebuchet
+  private var scale = SCALE
+  
   private var lever: Lever = _
   private var counterWeight: CounterWeight = _
   private var sling: Sling = _
   private var projectile: Projectile = _
-  private val forceFromHook = new Vector2d(0, 0)
-  private var partRenderers:Seq[AbstractPartRenderer] = Seq()
-  // scales the geometry of the trebuchet
-  private var scale = SCALE
   private var trebuchetRenderer: TrebuchetRenderer = _
+  private var trebuchetProcessor: TrebuchetProcessor = _
   
   commonInit()
 
@@ -66,92 +52,12 @@ class Trebuchet(var showVelocityVectors: Boolean = false, var showForceVectors: 
     projectile = new Projectile(base, DEFAULT_PROJECTILE_MASS)
     sling = Sling(lever, projectile, DEFAULT_SLING_LENGTH, DEFAULT_SLING_RELEASE_ANGLE)
     trebuchetRenderer = new TrebuchetRenderer(base, lever, counterWeight, sling, projectile)
+    trebuchetProcessor = new TrebuchetProcessor(lever, counterWeight, sling, projectile)
   }
 
-  /**
-    * steps the simulation forward in time
-    * if the timestep is too big inaccuracy and instability may result.
-    * @return the new timestep
-    */
   def stepForward(timeStep: Double): Double = {
-    //logger_.println(1, LOG_LEVEL, "stepForward: about to update (timeStep="+timeStep+')');
-    var angle = lever.getAngle
-    var angularVelocity = lever.getAngularVelocity
-    val slingAngle = sling.getAngleWithLever
-    val torque = calculateTorque(angle, slingAngle)
-    val inertia = calculateInertia
-    var angularAcceleration: Double = 0
-    if (angle < Trebuchet.MAX_LEVER_ANGLE) {
-      angularAcceleration = inertia / torque // in radians per second squared
-
-      angularVelocity += timeStep * angularAcceleration
-      angle += timeStep * angularVelocity
-    }
-    else {
-      angularVelocity = 0
-      angle = Trebuchet.MAX_LEVER_ANGLE
-    }
-    lever.setAngle(angle)
-    lever.setAngularVelocity(angularVelocity)
-
-    // calculate the forces acting on the projectile.
-    // the magnitude of the tangential force at the hook
-    if (!projectile.isReleased) {
-      val tangentialForceAtHook = torque / lever.getSlingLeverLength
-      //println("tangentialForceAtHook="+tangentialForceAtHook);
-      val slingAngleWithHorz = sling.getAngleWithHorz
-      forceFromHook.set(-cos(slingAngleWithHorz), sin(slingAngleWithHorz)) //sin(PI - angle), -cos(PI + angle));
-
-      forceFromHook.scale(tangentialForceAtHook * sin(slingAngle))
-      val gravityForce = new Vector2d(Trebuchet.GRAVITY_VEC)
-      gravityForce.scale(projectile.getMass)
-      forceFromHook.add(gravityForce)
-      // also add a restoring force which is proportional to the distance from the attachPoint on the sling
-      // if we have not yet been released.
-      val restoreForce = sling.getProjectileAttachPoint
-      restoreForce.sub(projectile.getPosition)
-      restoreForce.scale(100.0)
-      forceFromHook.add(restoreForce)
-      projectile.setForce(forceFromHook, timeStep)
-    }
-    else {
-      val gravityForce = new Vector2d(Trebuchet.GRAVITY_VEC)
-      gravityForce.scale(projectile.getMass)
-      projectile.setForce(gravityForce, timeStep)
-    }
-    // at the time when it is released, the only force acting on it will be gravity.
-    if (!projectile.isReleased && slingAngle >= (PI + sling.releaseAngle)) {
-      println("##########################################################################")
-      println("released!  slingAngle = " + slingAngle + " sling release angle = " + sling.releaseAngle)
-      println("##########################################################################")
-      projectile.isReleased = true
-    }
-    timeStep
+    trebuchetProcessor.stepForward(timeStep)
   }
-
-  private def calculateTorque(angle: Double, slingAngle: Double) = {
-    var primaryTorque: Double = 0
-    if (angle < Trebuchet.MAX_LEVER_ANGLE)
-      primaryTorque = lever.getCounterWeightLeverLength * sin(angle) * GRAVITY * counterWeight.getMass
-    var dragTorque: Double = 0
-    if (projectile.isOnRamp) { // case when the projectile is still on the ramp
-      dragTorque = -lever.getSlingLeverLength * projectile.getMass * GRAVITY * RAMP_FRICTION * sin(slingAngle)
-    }
-    else { // case when the projectile is no longer on the ramp
-      val r = projectile.getDistanceFrom(lever.getFulcrumPosition)
-      dragTorque = r * projectile.getMass * GRAVITY * cos(PI - angle - slingAngle) * sin(angle)
-    }
-    //println("torque= primaryTorque("+primaryTorque+")" +
-    //        " + dragTorque("+dragTorque+")= "+(primaryTorque+dragTorque));
-    primaryTorque + dragTorque
-  }
-
-  /**
-    * Got this from a physics text:
-    * I = LEVER_MASS / 3  (b^3 + c^3) + projectileMass/3 * r squared
-    * @return the calculated inertia
-    */
-  private def calculateInertia = lever.getInertia + projectile.getInertia(lever.getFulcrumPosition)
 
   def setScale(scale: Double): Unit = {
     this.scale = scale
---
+++ scala-source/com/barrybecker4/simulation/trebuchet/model/TrebuchetProcessor.scala
@@ -0,0 +1,107 @@
+// Copyright by Barry G. Becker, 2022. Licensed under MIT License: http://www.opensource.org/licenses/MIT
+package com.barrybecker4.simulation.trebuchet.model
+
+import com.barrybecker4.simulation.common.PhysicsConstants.GRAVITY
+import com.barrybecker4.simulation.trebuchet.model.TrebuchetConstants.RAMP_FRICTION
+import com.barrybecker4.simulation.trebuchet.model.TrebuchetProcessor.{GRAVITY_VEC, MAX_LEVER_ANGLE}
+import com.barrybecker4.simulation.trebuchet.model.parts.{CounterWeight, Lever, Projectile, Sling}
+
+import java.lang.Math.{PI, cos, sin}
+import javax.vecmath.Vector2d
+
+
+object TrebuchetProcessor {
+  private val GRAVITY_VEC = new Vector2d(0, GRAVITY)
+  private val MAX_LEVER_ANGLE = PI - 0.1
+}
+
+class TrebuchetProcessor(lever: Lever, counterWeight: CounterWeight, sling: Sling, projectile: Projectile) {
+
+  private val forceFromHook = new Vector2d(0, 0)
+
+  /**
+    * steps the simulation forward in time
+    * if the timestep is too big inaccuracy and instability may result.
+    * @return the new timestep
+    */
+  def stepForward(timeStep: Double): Double = {
+    //logger_.println(1, LOG_LEVEL, "stepForward: about to update (timeStep="+timeStep+')');
+    var angle = lever.getAngle
+    var angularVelocity = lever.getAngularVelocity
+    val slingAngle = sling.getAngleWithLever
+    val torque = calculateTorque(angle, slingAngle)
+    val inertia = calculateInertia
+    var angularAcceleration: Double = 0
+    if (angle < MAX_LEVER_ANGLE) {
+      angularAcceleration = inertia / torque // in radians per second squared
+
+      angularVelocity += timeStep * angularAcceleration
+      angle += timeStep * angularVelocity
+    }
+    else {
+      angularVelocity = 0
+      angle = MAX_LEVER_ANGLE
+    }
+    lever.setAngle(angle)
+    lever.setAngularVelocity(angularVelocity)
+
+    // calculate the forces acting on the projectile.
+    // the magnitude of the tangential force at the hook
+    if (!projectile.isReleased) {
+      val tangentialForceAtHook = torque / lever.getSlingLeverLength
+      //println("tangentialForceAtHook="+tangentialForceAtHook);
+      val slingAngleWithHorz = sling.getAngleWithHorz
+      forceFromHook.set(-cos(slingAngleWithHorz), sin(slingAngleWithHorz)) //sin(PI - angle), -cos(PI + angle));
+
+      forceFromHook.scale(tangentialForceAtHook * sin(slingAngle))
+      val gravityForce = new Vector2d(GRAVITY_VEC)
+      gravityForce.scale(projectile.getMass)
+      forceFromHook.add(gravityForce)
+      // also add a restoring force which is proportional to the distance from the attachPoint on the sling
+      // if we have not yet been released.
+      val restoreForce = sling.getProjectileAttachPoint
+      restoreForce.sub(projectile.getPosition)
+      restoreForce.scale(100.0)
+      forceFromHook.add(restoreForce)
+      projectile.setForce(forceFromHook, timeStep)
+    }
+    else {
+      val gravityForce = new Vector2d(GRAVITY_VEC)
+      gravityForce.scale(projectile.getMass)
+      projectile.setForce(gravityForce, timeStep)
+    }
+    // at the time when it is released, the only force acting on it will be gravity.
+    if (!projectile.isReleased && slingAngle >= (PI + sling.releaseAngle)) {
+      println("##########################################################################")
+      println("released!  slingAngle = " + slingAngle + " sling release angle = " + sling.releaseAngle)
+      println("##########################################################################")
+      projectile.isReleased = true
+    }
+    timeStep
+  }
+
+  private def calculateTorque(angle: Double, slingAngle: Double) = {
+    var primaryTorque: Double = 0
+    if (angle < MAX_LEVER_ANGLE)
+      primaryTorque = lever.getCounterWeightLeverLength * sin(angle) * GRAVITY * counterWeight.getMass
+    var dragTorque: Double = 0
+    if (projectile.isOnRamp) { // case when the projectile is still on the ramp
+      dragTorque = -lever.getSlingLeverLength * projectile.getMass * GRAVITY * RAMP_FRICTION * sin(slingAngle)
+    }
+    else { // case when the projectile is no longer on the ramp
+      val r = projectile.getDistanceFrom(lever.getFulcrumPosition)
+      dragTorque = r * projectile.getMass * GRAVITY * cos(PI - angle - slingAngle) * sin(angle)
+    }
+    //println("torque= primaryTorque("+primaryTorque+")" +
+    //        " + dragTorque("+dragTorque+")= "+(primaryTorque+dragTorque));
+    primaryTorque + dragTorque
+  }
+
+  /**
+    * Got this from a physics text:
+    * I = LEVER_MASS / 3  (b^3 + c^3) + projectileMass/3 * r squared
+    * @return the calculated inertia
+    */
+  private def calculateInertia = lever.getInertia + projectile.getInertia(lever.getFulcrumPosition)
+
+}
